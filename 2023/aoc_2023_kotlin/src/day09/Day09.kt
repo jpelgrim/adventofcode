de666fa7107010a9f3f0545a46786ef3e99bc9a7
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-09 09:52:06 +0100

    Day 9 🏜️

--- 2023/aoc_2023_kotlin/src/day09/Day09.kt
+++ 2023/aoc_2023_kotlin/src/day09/Day09.kt
@@ -3,32 +3,34 @@ package day09
 import println
 import readLines
 
-private const val DAY = "day09"
+private const val DAY = "day09" // https://adventofcode.com/2023/day/9
 
 fun main() {
     solveDay9()
 }
 
 fun solveDay9() {
-    val inputPart1 =
-        "$DAY/part1_example.txt"
-//        "$DAY/part1.txt"
-            .readLines()
-    val solutionPart1 = solvePart1(inputPart1)
-    "The solution for $DAY part1 is: $solutionPart1".println()
-
-    val inputPart2 =
-        "$DAY/part2_example.txt"
-//        "$DAY/part2.txt"
-            .readLines()
-    val solutionPart2 = solvePart2(inputPart2)
-    "The solution for $DAY part2 is: $solutionPart2".println()
+    val input = "$DAY/input_example.txt".readLines()
+    "The solution for $DAY part1 is: ${solvePart1(input)}".println()
+    "The solution for $DAY part2 is: ${solvePart2(input)}".println()
 }
 
-fun solvePart1(input: List<String>): Int {
-    TODO()
+fun solvePart1(input: List<String>) = input.sumOf {
+    it.split(" ").map { number -> number.toInt() }.predictNext()
 }
 
-fun solvePart2(input: List<String>): Int {
-    TODO()
+fun solvePart2(input: List<String>) = input.sumOf {
+    it.split(" ").map { number -> number.toInt() }.predictFirst()
 }
+
+private fun List<Int>.predictNext(): Int {
+    val diffs = this.differences()
+    return this.last() + if (diffs.all { it == diffs[0] }) diffs[0] else diffs.predictNext()
+}
+
+private fun List<Int>.predictFirst(): Int {
+    val diffs = this.differences()
+    return this.first() - if (diffs.all { it == diffs[0] }) diffs[0] else diffs.predictFirst()
+}
+
+private fun List<Int>.differences() = (1..this.lastIndex).map { i -> this[i] - this[i - 1] }
