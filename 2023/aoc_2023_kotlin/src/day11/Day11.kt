aea8c3d3966b3eeaf94c83ba3c0e5a6ef8454f81
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-11 09:17:50 +0100

    Day 11 🌌

--- 2023/aoc_2023_kotlin/src/day11/Day11.kt
+++ 2023/aoc_2023_kotlin/src/day11/Day11.kt
@@ -2,33 +2,63 @@ package day11
 
 import println
 import readLines
+import kotlin.math.abs
 
-private const val DAY = "day11"
+private const val DAY = "day11" // https://adventofcode.com/2023/day/11
 
 fun main() {
     solveDay11()
 }
 
+data class Point(val x: Long, val y: Long)
+
 fun solveDay11() {
-    val inputPart1 =
-        "$DAY/part1_example.txt"
-//        "$DAY/part1.txt"
-            .readLines()
-    val solutionPart1 = solvePart1(inputPart1)
-    "The solution for $DAY part1 is: $solutionPart1".println()
+    val input = "$DAY/input_example.txt".readLines()
 
-    val inputPart2 =
-        "$DAY/part2_example.txt"
-//        "$DAY/part2.txt"
-            .readLines()
-    val solutionPart2 = solvePart2(inputPart2)
-    "The solution for $DAY part2 is: $solutionPart2".println()
+    val emptyColumns = (0..<input[0].length).map { it }.toMutableList()
+    val emptyRows = mutableListOf<Int>()
+    val galaxies = mutableListOf<Point>()
+    input.forEachIndexed { y, line ->
+        if (line.all { it == '.' }) {
+            emptyRows.add(y)
+        } else {
+            line.forEachIndexed { x, c ->
+                if (c == '#') {
+                    emptyColumns.remove(x)
+                    galaxies.add(Point(x.toLong(), y.toLong()))
+                }
+            }
+        }
+    }
+    val translatedGalaxies = translateGalaxies(galaxies, emptyColumns, emptyRows, 2)
+    "The solution for $DAY part1 is: ${manhattanDistances(translatedGalaxies).sum()}".println()
+
+    val translatedGalaxiesPart2 = translateGalaxies(galaxies, emptyColumns, emptyRows, 1000000)
+    "The solution for $DAY part2 is: ${manhattanDistances(translatedGalaxiesPart2).sum()}".println()
 }
 
-fun solvePart1(input: List<String>): Int {
-    TODO()
+private fun translateGalaxies(
+    galaxies: MutableList<Point>,
+    emptyColumns: MutableList<Int>,
+    emptyRows: MutableList<Int>,
+    expansion: Int
+): List<Point> {
+    val translatedGalaxies = galaxies.map { point ->
+        val x = point.x
+        val y = point.y
+        val translateX = emptyColumns.count { it < x } * (expansion - 1)
+        val translateY = emptyRows.count { it < y } * (expansion - 1)
+        Point(x + translateX, y + translateY)
+    }
+    return translatedGalaxies
 }
 
-fun solvePart2(input: List<String>): Int {
-    TODO()
+private fun manhattanDistances(translatedGalaxies: List<Point>): MutableList<Long> {
+    val distances = mutableListOf<Long>()
+    translatedGalaxies.forEachIndexed { index, point ->
+        translatedGalaxies.subList(index + 1, translatedGalaxies.size).forEach { otherPoint ->
+            distances.add(abs(point.x - otherPoint.x) + abs(point.y - otherPoint.y))
+        }
+    }
+    return distances
 }
