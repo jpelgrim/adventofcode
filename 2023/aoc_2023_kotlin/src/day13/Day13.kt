51ceb3c318d5e103aa7a5022e20545b688420e4e
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-13 09:10:01 +0100

    Day 13 🪞

--- 2023/aoc_2023_kotlin/src/day13/Day13.kt
+++ 2023/aoc_2023_kotlin/src/day13/Day13.kt
@@ -1,7 +1,7 @@
 package day13
 
 import println
-import readLines
+import readText
 
 private const val DAY = "day13"
 
@@ -10,25 +10,71 @@ fun main() {
 }
 
 fun solveDay13() {
-    val inputPart1 =
-        "$DAY/part1_example.txt"
-//        "$DAY/part1.txt"
-            .readLines()
-    val solutionPart1 = solvePart1(inputPart1)
-    "The solution for $DAY part1 is: $solutionPart1".println()
-
-    val inputPart2 =
-        "$DAY/part2_example.txt"
-//        "$DAY/part2.txt"
-            .readLines()
-    val solutionPart2 = solvePart2(inputPart2)
-    "The solution for $DAY part2 is: $solutionPart2".println()
+    val input = "$DAY/input_example.txt".readText().split("\n\n").map { it.split("\n") }
+    "The solution for $DAY part1 is: ${solvePart1(input)}".println()
+    "The solution for $DAY part2 is: ${solvePart2(input)}".println()
 }
 
-fun solvePart1(input: List<String>): Int {
-    TODO()
+fun solvePart1(matrices: List<List<String>>) = matrices.sumOf { listOfRows ->
+    findMirrorIndex(listOfRows).let {  if (it > 0) return@sumOf it * 100 }
+    findMirrorIndex(transpose(listOfRows)).let {  if (it > 0) return@sumOf it }
+    return@sumOf 0
 }
 
-fun solvePart2(input: List<String>): Int {
-    TODO()
+fun solvePart2(matrices: List<List<String>>) = matrices.sumOf { listOfRows ->
+    findMirrorIndexFuzzy(listOfRows).let {  if (it > 0) return@sumOf it * 100 }
+    findMirrorIndexFuzzy(transpose(listOfRows)).let {  if (it > 0) return@sumOf it }
+    return@sumOf 0
+}
+
+fun transpose(list: List<String>): List<String> {
+    val transposed = mutableListOf<String>()
+    val width = list[0].length
+    for (i in 0..<width) {
+        val sb = StringBuilder()
+        for (j in list.indices) {
+            sb.append(list[j][i])
+        }
+        transposed.add(sb.toString())
+    }
+    return transposed
+}
+
+private fun findMirrorIndex(
+    list: List<String>,
+): Int {
+    (1..<list.size).forEach { index ->
+        var matchFound = true
+        for (i in 0..index) {
+            if (index + i >= list.size || index - i - 1 < 0) break
+            if (list[index + i] != list[index - i - 1]) {
+                matchFound = false
+                break
+            }
+        }
+        if (matchFound) return index
+    }
+    return 0
+}
+
+private fun findMirrorIndexFuzzy(
+    list: List<String>,
+): Int {
+    (1..<list.size).forEach { index ->
+        var charsDifferent = 0
+        for (i in 0..index) {
+            if (index + i >= list.size || index - i - 1 < 0) break
+            charsDifferent += charsDifferent(list[index + i], list[index - i - 1])
+        }
+        if (charsDifferent == 1) return index
+    }
+    return 0
+}
+
+fun charsDifferent(first: String, second: String): Int {
+    var count = 0
+    for (i in first.indices) {
+        if (first[i] != second[i]) count++
+    }
+    return count
 }
