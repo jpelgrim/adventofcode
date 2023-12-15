961d9736e780caedfda67a3634be2d8b2dd4da24
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-15 08:23:53 +0100

    Day 15 🔎

--- 2023/aoc_2023_kotlin/src/day15/Day15.kt
+++ 2023/aoc_2023_kotlin/src/day15/Day15.kt
@@ -1,34 +1,57 @@
 package day15
 
 import println
-import readLines
+import readText
 
-private const val DAY = "day15"
+private const val DAY = "day15" // https://adventofcode.com/2023/day/15
 
 fun main() {
     solveDay15()
 }
 
 fun solveDay15() {
-    val inputPart1 =
-        "$DAY/part1_example.txt"
-//        "$DAY/part1.txt"
-            .readLines()
-    val solutionPart1 = solvePart1(inputPart1)
+    val exampleInput = "$DAY/input_example.txt".readText().split(",")
+    val solutionPart1 = solvePart1(exampleInput)
+    check(solutionPart1 == 1320)
     "The solution for $DAY part1 is: $solutionPart1".println()
 
-    val inputPart2 =
-        "$DAY/part2_example.txt"
-//        "$DAY/part2.txt"
-            .readLines()
-    val solutionPart2 = solvePart2(inputPart2)
+    val solutionPart2 = solvePart2(exampleInput)
+    check(solutionPart2 == 145)
     "The solution for $DAY part2 is: $solutionPart2".println()
 }
 
-fun solvePart1(input: List<String>): Int {
-    TODO()
-}
+fun String.hashAlgorithm() = fold(0) { acc, i -> (acc + i.code) * 17 % 256 }
+
+fun solvePart1(input: List<String>) = input.sumOf { it.hashAlgorithm() }
 
 fun solvePart2(input: List<String>): Int {
-    TODO()
+    val map = mutableMapOf<Int, MutableList<Pair<String, Int>>>()
+    input.forEach { step ->
+        if (step.contains("=")) {
+            val (label, value) = step.split("=")
+            val labelHash = label.hashAlgorithm()
+            val list = map[labelHash] ?: mutableListOf()
+            if (list.any { it.first == label }) {
+                val existingPair = list.first { it.first == label }
+                val index = list.indexOf(existingPair)
+                list.remove(existingPair)
+                list.add(index, Pair(label, value.toInt()))
+            } else {
+                list.add(Pair(label, value.toInt()))
+            }
+            if (map[labelHash] == null) map[labelHash] = list
+        } else {
+            val (label, _) = step.split("-")
+            val labelHash = label.hashAlgorithm()
+            map[labelHash]?.let { list ->
+                list.firstOrNull { it.first == label }?.let { list.remove(it) }
+                if (list.isEmpty()) map.remove(labelHash)
+            }
+        }
+    }
+    return map.entries.sumOf { entry ->
+        entry.value.foldIndexed(0) { index, sum, pair ->
+            sum + (pair.second * (index + 1) * (entry.key + 1))
+        }.toInt()
+    }
 }
