384fdb1b8d2c8d5cffb667b988803456c03df7d7
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-16 15:03:38 +0100

    Day 16 🪞

--- 2023/aoc_2023_kotlin/src/day16/Day16.kt
+++ 2023/aoc_2023_kotlin/src/day16/Day16.kt
@@ -1,34 +1,106 @@
 package day16
 
+import day16.Direction.DOWN
+import day16.Direction.LEFT
+import day16.Direction.RIGHT
+import day16.Direction.UP
 import println
 import readLines
 
-private const val DAY = "day16"
+private const val DAY = "day16" // https://adventofcode.com/2023/day/16
 
 fun main() {
     solveDay16()
 }
 
 fun solveDay16() {
-    val inputPart1 =
-        "$DAY/part1_example.txt"
-//        "$DAY/part1.txt"
-            .readLines()
-    val solutionPart1 = solvePart1(inputPart1)
+    val input = "$DAY/input_example.txt".readLines()
+    val width = input[0].length
+    val height = input.size
+
+    val solutionPart1 = solvePart1(input, width, height)
+    check(solutionPart1 == 46)
     "The solution for $DAY part1 is: $solutionPart1".println()
 
-    val inputPart2 =
-        "$DAY/part2_example.txt"
-//        "$DAY/part2.txt"
-            .readLines()
-    val solutionPart2 = solvePart2(inputPart2)
+    val solutionPart2 = solvePart2(input, width, height)
+    check(solutionPart2 == 51)
     "The solution for $DAY part2 is: $solutionPart2".println()
 }
 
-fun solvePart1(input: List<String>): Int {
-    TODO()
+internal fun solvePart1(lines: List<String>, width: Int, height: Int) =
+    calcEnergized(lines, width, height, Point(0, 0) to RIGHT)
+
+fun solvePart2(input: List<String>, width: Int, height: Int): Int {
+    var maxEnergized = 0
+    for (x in input[0].indices) {
+        var energized = calcEnergized(input, width, height, Point(x, 0) to DOWN)
+        if (energized > maxEnergized) {
+            maxEnergized = energized
+        }
+        energized = calcEnergized(input, width, height, Point(x, height - 1) to UP)
+        if (energized > maxEnergized) {
+            maxEnergized = energized
+        }
+    }
+    for (y in input.indices) {
+        var energized = calcEnergized(input, width, height, Point(0, y) to RIGHT)
+        if (energized > maxEnergized) {
+            maxEnergized = energized
+        }
+        energized = calcEnergized(input, width, height, Point(width - 1, y) to LEFT)
+        if (energized > maxEnergized) {
+            maxEnergized = energized
+        }
+    }
+    return maxEnergized
 }
 
-fun solvePart2(input: List<String>): Int {
-    TODO()
+private fun calcEnergized(
+    lines: List<String>, width: Int, height: Int, start: Pair<Point, Direction>
+): Int {
+    val stack = mutableListOf(start)
+    val visited = mutableSetOf(start)
+
+    while (true) {
+        val (currentPoint, direction) = stack.removeFirstOrNull() ?: break
+        val mirror = lines[currentPoint.y][currentPoint.x]
+        for (newDirection in mirror.reflectedDirections(direction)) {
+            val newPoint = currentPoint.move(newDirection)
+            if (newPoint.withinBounds(width, height)) {
+                val newPointDirectionPair = newPoint to newDirection
+                if (visited.add(newPointDirectionPair)) stack.add(newPointDirectionPair)
+            }
+        }
+    }
+
+    return visited.mapTo(mutableSetOf()) { it.first }.size
+}
+
+internal enum class Direction { UP, DOWN, LEFT, RIGHT; }
+
+internal data class Point(val x: Int, val y: Int) {
+
+    fun move(direction: Direction) = when (direction) {
+        UP -> Point(x, y - 1)
+        DOWN -> Point(x, y + 1)
+        RIGHT -> Point(x + 1, y)
+        LEFT -> Point(x - 1, y)
+    }
+
+    fun withinBounds(width: Int, height: Int) = x in 0 until width && y in 0 until height
+
+}
+
+internal fun Char.reflectedDirections(direction: Direction): List<Direction> = when {
+    this == '/' && direction == UP -> listOf(RIGHT)
+    this == '/' && direction == DOWN -> listOf(LEFT)
+    this == '/' && direction == RIGHT -> listOf(UP)
+    this == '/' && direction == LEFT -> listOf(DOWN)
+    this == '\\' && direction == UP -> listOf(LEFT)
+    this == '\\' && direction == DOWN -> listOf(RIGHT)
+    this == '\\' && direction == RIGHT -> listOf(DOWN)
+    this == '\\' && direction == LEFT -> listOf(UP)
+    this == '-' && (direction == UP || direction == DOWN) -> listOf(LEFT, RIGHT)
+    this == '|' && (direction == RIGHT || direction == LEFT) -> listOf(UP, DOWN)
+    else -> listOf(direction) // A '.' or parallel mirror so keep going in the same direction
 }
