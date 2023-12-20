3fedf33c90f3d4e53ca868164a1358a56e6db0b4
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-20 10:50:26 +0100

    Day 20 🛜
    Oops forgot to check in the lcm function to calculate the lowest common multiplier for a list of Long numbers

--- 2023/aoc_2023_kotlin/src/util/Math.kt
+++ 2023/aoc_2023_kotlin/src/util/Math.kt
@@ -4,3 +4,7 @@ fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 fun lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
 fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
 fun lcm(a: Long, b: Long): Long = a * (b / gcd(a, b))
+fun lcm(numbers: List<Long>): Long {
+    val gcd = numbers.reduce { acc, l -> gcd(acc, l) }
+    return numbers.reduce { acc, l -> lcm(acc, l) } / gcd
+}
