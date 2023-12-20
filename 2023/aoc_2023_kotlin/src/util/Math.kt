103ddbfeb3a8ab9f37401da102f694a0e9067b39
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-20 10:53:44 +0100

    Day 20 🛜
    Added lcm for a list of Int numbers

--- 2023/aoc_2023_kotlin/src/util/Math.kt
+++ 2023/aoc_2023_kotlin/src/util/Math.kt
@@ -1,9 +1,20 @@
+@file:Suppress("unused")
+
 package util
 
 fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
 fun lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
+
+fun lcm(numbers: List<Int>): Int {
+    val gcd = numbers.reduce { acc, l -> gcd(acc, l) }
+    return numbers.reduce { acc, l -> lcm(acc, l) } / gcd
+}
+
 fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+
 fun lcm(a: Long, b: Long): Long = a * (b / gcd(a, b))
+
 fun lcm(numbers: List<Long>): Long {
     val gcd = numbers.reduce { acc, l -> gcd(acc, l) }
     return numbers.reduce { acc, l -> lcm(acc, l) } / gcd
