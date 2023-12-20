2e0d0837092fa57524671e2e439fe3eceab8fb7e
Johan Pelgrim <johan.pelgrim@gmail.com>
2023-12-20 10:55:08 +0100

    Day 20 🛜
    Simplified lcm functions for a list of numbers

--- 2023/aoc_2023_kotlin/src/util/Math.kt
+++ 2023/aoc_2023_kotlin/src/util/Math.kt
@@ -6,16 +6,12 @@ fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
 fun lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
 
-fun lcm(numbers: List<Int>): Int {
-    val gcd = numbers.reduce { acc, l -> gcd(acc, l) }
-    return numbers.reduce { acc, l -> lcm(acc, l) } / gcd
-}
+fun lcm(numbers: List<Int>) =
+    numbers.reduce { acc, l -> lcm(acc, l) } / numbers.reduce { acc, l -> gcd(acc, l) }
 
 fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
 
 fun lcm(a: Long, b: Long): Long = a * (b / gcd(a, b))
 
-fun lcm(numbers: List<Long>): Long {
-    val gcd = numbers.reduce { acc, l -> gcd(acc, l) }
-    return numbers.reduce { acc, l -> lcm(acc, l) } / gcd
-}
+fun lcm(numbers: List<Long>) =
+    numbers.reduce { acc, l -> lcm(acc, l) } / numbers.reduce { acc, l -> gcd(acc, l) }
